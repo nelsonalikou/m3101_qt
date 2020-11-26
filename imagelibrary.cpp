ff28b5e6c22419df153318436b402815e311681f
alik0002 <nelson.alikou-dongmo@etudiant.univ-reims.fr>
2020-11-26 13:00:11 +0100

    Début édition miniatures asynchrones(non fonctionnel

--- imagelibrary.cpp
+++ imagelibrary.cpp
@@ -8,6 +8,9 @@
 #include <QThread>
 #include <QtConcurrent>
 #include <QFuture>
+#include <QList>
+#include <QThread>
+#include <QListView>
 #include "imagelibrary.h"
 
 
@@ -17,7 +20,11 @@ ImageLibrary::ImageLibrary(QWidget *parent)
     this->view.setModel(&model);
     this->toolbar.addAction("GO !", this, &ImageLibrary::go);
     addToolBar(&toolbar);
+
+    this->view.setViewMode(QListView::IconMode);
+    this->view.setGridSize(QSize (THUMBNAIL_SIZE, THUMBNAIL_SIZE));
     setCentralWidget(&view);
+
 }
 
 ImageLibrary::~ImageLibrary()
@@ -40,6 +47,27 @@ void ImageLibrary::go()
     {
         settings.setValue("start",dir);
         QMessageBox::information(this, "Info","Success");
+
+
+        QThread * thread = new QThread;
+         //Worker worker(dir);
+        Worker * worker = new Worker (dir);
+        worker->moveToThread (thread);
+
+
+
+        connect(worker, &Worker::newItem, &model, &Model::addItem);
+
+        connect (thread, &QThread::started, worker, &Worker::process);
+        connect (worker, &Worker::finished, thread, &QThread::quit);
+        connect (worker, &Worker::finished, worker, &Worker::deleteLater);
+        connect (thread, &QThread::finished, thread, &QThread::deleteLater);
+        thread->start ();
+
+        connect(worker, &Worker::newThumbnail, &model, &Model::setThumbnail);
+        worker->process();
+
+        settings.setValue("lastDir", dir);
     }
 
 
@@ -54,18 +82,6 @@ void ImageLibrary::go()
     //work.process();
 
 
-    /*QThread * thread = new QThread;
-    Worker * worker = new Worker (dir);
-    worker->moveToThread (thread);
-    connect (thread, &QThread::started, worker, &Worker::process);
-    connect (worker, &Worker::finished, thread, &QThread::quit);
-    connect (worker, &Worker::finished, worker, &Worker::deleteLater);
-    connect (thread, &QThread::finished, thread, &QThread::deleteLater);
-    thread->start ();
-    connect(worker, &Worker::newItem, this, &ImageLibrary::addItem);
-    */
-    //QtConcurrent::run(worker, &Worker::process);
-
     QtConcurrent::run([=](){
         Worker worker(dir);
         connect(&worker, &Worker::newItem, this, &ImageLibrary::addItem);
@@ -75,8 +91,16 @@ void ImageLibrary::go()
 }
 
 
+//Fonction d'ajout des items dans la librairie
+/*void ImageLibrary::addItem (const QString & item)
+{
+  QStringList list = model.stringList ();
+  model.setStringList (list << item);
+}
+*/
 
-Worker::Worker(const QString &path) : QObject(),path(path)
+
+Worker::Worker(const QString &path) : QObject(),path(path), watcher()
 {
 
 }
@@ -86,11 +110,15 @@ Worker::~Worker()
 
 }
 
+
+//Methode de recuperation des images et d'emissions des signaux
 void Worker::process()
 {
     //File d'attente
     QStringList wait;
     wait << this->path;
+
+    QStringList paths;
 
     // Edition des filtres
     QStringList filters;
@@ -125,15 +153,86 @@ void Worker::process()
       {
           //std::cout << info.absoluteFilePath().toStdString() << std::endl;
           emit newItem(info.absoluteFilePath());
+          paths << info.absoluteFilePath();
       }
     }
+    connect(&this->watcher, &QFutureWatcher<Item>::finished, this, &Worker::finished);
+    connect(&watcher, &QFutureWatcher<Item>::resultReadyAt, this, &Worker::processItem);
+    watcher.setFuture( QtConcurrent::mapped(path, Worker::MappedItem));
 
 }
 
 
-
-void ImageLibrary::addItem (const QString & item)
+//Fonction de création de la miniature das le worker
+QImage Worker::Thumbnail(const QString & path)
 {
-  QStringList list = model.stringList ();
-  model.setStringList (list << item);
+    QImage img (path);
+    return img.scaled(QSize (THUMBNAIL_SIZE, THUMBNAIL_SIZE), Qt::KeepAspectRatio);
+}
+
+
+Item Worker::MappedItem(const QString & path)
+{
+    //std::cout << "MappedItem " << path.toStdString() << std::endl;
+    Item item(path, Worker::Thumbnail(path));
+    return item;
+}
+
+
+void Worker::processItem(int k)
+{
+    std::cout << "processItem " << k << std::endl;
+    emit newThumbnail( k, watcher.resultAt(k).thumbnail);
+}
+
+
+
+
+
+Item::Item(const QString &path, const QImage &thumbnail) : path(path),thumbnail(thumbnail)
+{
+
+}
+
+Item::~Item()
+{
+
+}
+
+
+Model::Model() : items()
+{
+
+}
+
+
+Model::~Model()
+{
+
+}
+
+
+int Model::rowCount(const QModelIndex &index) const
+{
+    /*int row = 0;
+    if (index.isValid())
+    {
+        row = index.row();
+    }
+    return row;*/
+    return this->items.size();
+}
+
+
+QVariant Model::data(const QModelIndex &index, int role) const{
+    return index.data(role);
+}
+
+
+void Model::addItem(const QString &path, const QImage &thumbnail)
+{
+    Item item (path,thumbnail);
+    beginInsertRows(QModelIndex (), rowCount(), rowCount());
+    this->items << item;
+    endInsertRows();
 }
--- imagelibrary.h
+++ imagelibrary.h
@@ -1,6 +1,6 @@
 #ifndef IMAGELIBRARY_H
 #define IMAGELIBRARY_H
-
+#define THUMBNAIL_SIZE 128
 #include <QMainWindow>
 #include <qstringlistmodel.h>
 #include <qlistview.h>
@@ -12,7 +12,39 @@
 #include <QThread>
 #include <QtConcurrent>
 #include <iostream>
+#include <QFuture>
+#include <QList>
 
+// Classe Item
+class Item
+{
+private:
+    QString path;
+    QImage thumbnail;
+public:
+    Item(const QString &, const QImage &);
+    ~Item();
+};
+
+
+
+class Model : public QAbstractListModel
+{
+    Q_OBJECT
+private:
+    QList<Item> items;
+public:
+    Model();
+    ~Model();
+    int rowCount(const QModelIndex &parent = QModelIndex()) const;
+    QVariant data(const QModelIndex &, int = Qt::DisplayRole) const;
+
+public slots:
+    void addItem (const QString &, const QImage &);
+    void setThumbnail (int, const QImage &);
+};
+
+// Classe Image Library
 class ImageLibrary : public QMainWindow
 {
     Q_OBJECT
@@ -30,18 +62,30 @@ public slots:
     void addItem (const QString &);
 };
 
+
+//Classe Worker
 class Worker : public QObject
 {
     Q_OBJECT
 private:
     QString path;
+    QFutureWatcher<Item> watcher;
 public:
     Worker(const QString &);
     ~Worker();
     void process();
+    static Item MappedItem(const QString & path);
+    void processItem(int);
+    static QImage Thumbnail(const QString &);
+
 signals:
     void newItem(const QString &);
     void finished();
+    void newThumbnail(int, const QImage &);
+
+
 };
+
+
 
 #endif // IMAGELIBRARY_H
